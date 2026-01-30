6a11625358cfd23e533f13b69132e6dced622450
JBarbosa369 <jeovana.nascimento@aluno.senai.br>
2026-01-30 10:36:36 -0500

    Sabor atividade cxoncluidas

--- controller/controlador.php
+++ controller/controlador.php
@@ -1,5 +1,34 @@
 <?php
 include "./data/animals.php";
+function mainpage(){
+    global $items;
+    $banner = "./images/allanimals.jpg";
+    $title = "Todos os animais";
+    $content = $items;
+    include "./include/layout.php";
+}
 
-print_r($items);
+function gatospage(){
+    $banner = "./imagens/allanimais.jpg";
+    $title = "gatos";
+    $content = $items;
+
+    include "./include/layout.php";
+}
+
+function cachorrospage(){
+    $banner = "./imagens/allanimais.jpg";
+    $title = "cachorros";
+    $content = $items;
+
+    include "./include/layout.php";
+}
+
+function peixespage(){
+    $banner = "./imagens/allanimais.jpg";
+    $title = "peixes";
+    $content = $items;
+
+    include "./include/layout.php";
+}
 ?>
--- routes/rotas.php
+++ routes/rotas.php
@@ -4,18 +4,23 @@ echo $URL;
 include "./controller/controlador.php";
 if ($URL == "/atividade/"){
     echo "Entrou no If";
+    mainpage();
 }
 else if($URL == "/atividade/gatos"){
     echo "Rotas de gatos";
+    gatospage();
 }
 else if($URL == "/atividade/CaXorros"){
     echo "Rotas de caXorros";
+    cachorrospaige();
 }
 else if($URL == "/atividade/peixes"){
    echo "Rotas de peixes"; 
+   peixespaige();
 }
 else if($URL == "/atividadepesquisa"){
     echo "Rotas de pesquisa";
+    pesquisapaige();
 }
 else{
     echo "NOT FOUND!!";
